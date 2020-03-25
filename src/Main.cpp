375d605503c27496fecaebced26ea46135a8cedd
Nopharut CHADEE <nchadee@etud.u-pem.fr>
2020-03-25 12:28:30 +0100

    Update

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,11 +12,11 @@ const string W_NAME = "Main";
 constexpr unsigned int W_WIDTH = 800;
 constexpr unsigned int W_HEIGHT = 600;
 
-struct Ball2D {
-	int x;
-	int y;
-	int radius;
-};
+bool mousePressed = false;
+int mouseX = 0, mouseY = 0, radius = 20;
+int obj_ID = 1;
+
+Mat wMat(W_HEIGHT, W_WIDTH, CV_8UC3);
 
 template<typename T>
 Mvec<T> point(const T &x, const T &y) {
@@ -33,11 +33,35 @@ Mvec<T> point(const Mvec<T> &vec){
     return point(vec[E1], vec[E2]);
 }
 
+template<typename T>
+bool intersect(const Mvec<T> & mv1, const Mvec<T> & mv2) {
+	Mvec<double> tmp = (mv1.dual() ^ mv2.dual());
+	double norm = tmp.dual().norm();
+	cout << norm * norm << endl;
+	return (norm * norm >= 0);
+}
+
+struct Ball2D {
+	int x;
+	int y;
+	int radius;
+	int id;
+};
+
 vector<Ball2D> balls(0);
 vector<Mvec<double>> objects(0);
-Mat wMat(W_HEIGHT, W_WIDTH, CV_8UC3);
-bool mousePressed = false;
-int mouseX = 0, mouseY = 0, radius = 20;
+
+bool detect_collisions(Ball2D ball) {
+	Mvec<double> mv = objects[ball.id];
+	for(int i = 0; i < (int)objects.size(); i++) {
+		if(i != ball.id) {
+			if(intersect(mv, objects[i])) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
 
 void mouseCallback(int event, int x, int y, int flags, void *userdata = 0) {
 	if(event == EVENT_FLAG_LBUTTON) {
@@ -50,22 +74,22 @@ void mouseCallback(int event, int x, int y, int flags, void *userdata = 0) {
 
 void draw() {
 	wMat = Mat::zeros(W_HEIGHT, W_WIDTH, CV_8UC3);
-	for(Ball2D ball : balls) {
+	for(Ball2D & ball : balls) {
 		circle(wMat, Point(ball.x, ball.y), ball.radius, Scalar(0, 0, 255), FILLED);
 	}
 	imshow(W_NAME, wMat);
 }
 
 int main(int argc, char *argv[]) {
-	int key = -1;
-	bool done = false;
-	objects.push_back(point(0., 0.) ^ point(0., (double)W_HEIGHT));
-	objects.push_back(point(0., (double)W_WIDTH) ^ point((double)W_WIDTH, (double)W_HEIGHT));
-
 	namedWindow(W_NAME);
 	resizeWindow(W_NAME, W_WIDTH, W_HEIGHT);
 	setMouseCallback(W_NAME, mouseCallback);
 
+	Mvec<double> ground = point(0., (double)W_HEIGHT) ^ point((double)W_WIDTH, (double)W_HEIGHT);
+	objects.push_back(ground);
+
+	int key = -1;
+	bool done = false;
 	while(!done) {
 		if(getWindowProperty(W_NAME, WND_PROP_AUTOSIZE) == -1) {
 			done = true;
@@ -74,7 +98,12 @@ int main(int argc, char *argv[]) {
 
 		if((mousePressed == true) || ((key = waitKey(10)) != -1)) {
 			if(mousePressed == true) {
-				balls.push_back({mouseX, mouseY, radius});
+				balls.push_back({mouseX, mouseY, radius, obj_ID++});
+				objects.push_back(
+					point(mouseX, mouseY + radius) ^
+					point(mouseX, mouseY - radius) ^
+					point(mouseX + radius, mouseY)
+				);
 				mousePressed = false;
 			}
 			if(key != -1) {
@@ -85,7 +114,8 @@ int main(int argc, char *argv[]) {
 					case 'f':
 						cout << "You're paying respects...RIP" << endl; break;
 					case 32 :
-						balls.clear(); break;
+						balls.clear(); objects.clear(); objects.push_back(ground); obj_ID = 1; 
+						break;
 					default:
 						break;
 				}
@@ -94,9 +124,15 @@ int main(int argc, char *argv[]) {
 		}
 
 		for(Ball2D & ball : balls) {
-			//if(ball.y + radius < W_HEIGHT) {
-				ball.y = (ball.y + 1) % W_HEIGHT;
-			//}
+			if(ball.y + ball.radius < W_HEIGHT) {
+				ball.y++;
+			}
+			//ball.y = (ball.y + 1) % W_HEIGHT;
+			
+			/*
+			if(!detect_collisions(ball)) {
+				ball.y++;
+			}*/
 		}
 
 		draw();
